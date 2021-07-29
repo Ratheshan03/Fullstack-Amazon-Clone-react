bfea62c3c08818b77ecccd6973dd6943fe7e1040
Ratheshan03 <lionratheshan@gmail.com>
2021-07-29 22:09:48 +0530

    Payment process almost done

---
+++ functions/index.js
@@ -0,0 +1,39 @@
+const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
+const stripe = require("stripe")(
+  "sk_test_51JIXHDSEsTYCkzjC6InEQQVBMWhU7xV2EPuf7iz8LttubR2GcEoKF16ywSM1lem33efy4GmaZV6KO5injqOhbVnv00s8T0eglG"
+);
+
+// API
+
+// - App config
+const app = express();
+
+// - Middlewares
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+// - API Routes
+app.get("/", (request, response) => response.status(200).send("hello world"));
+
+app.post("/payments/create", async (request, response) => {
+  const total = request.query.total;
+
+  console.log("Payment Request Recieved BOOM!!! for this amount >>> ", total);
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: total, // subunits of the currency
+    currency: "inr",
+  });
+
+  // OK - created....
+  response.status(201).send({
+    clientSecret: paymentIntent.client_secret,
+  });
+});
+
+// - Listen command
+exports.api = functions.https.onRequest(app);
+
+// http://localhost:5001/clone-b18f7/us-central1/api
